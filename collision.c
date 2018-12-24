78bf853a00355ab4b0357878f68c7fc86e0b5f7d
MHN <mhn_1991@yahoo.com>
2018-12-24 07:39:52 +0000

    adding ability to find collision for any given nibbles

--- collision.c
+++ collision.c
@@ -4,12 +4,12 @@
 #include<string.h>
 #include<locale.h>
 
-void H(unsigned char * data, unsigned char *hash){
-  SHA1(data,10,hash);
+void H(unsigned char * data, unsigned char *hash,int nibbles){
+  SHA1(data,nibbles,hash);
 }
 
-void print(unsigned char *hashString){
-  for(int i=0;i<10;i++){
+void print(unsigned char *hashString,int nibbles){
+  for(int i=0;i<nibbles;i++){
     printf("%c",hashString[i]);
   }
 }
@@ -26,8 +26,8 @@ void ShaToString(unsigned char *hash,unsigned char *hashString)
   }
 }
 
-int compare(unsigned char *a,unsigned char *b){
-  for(int i=0;i<10;i++){
+int compare(unsigned char *a,unsigned char *b,int nibbles){
+  for(int i=0;i<nibbles;i++){
     if(a[i] != b[i]){
       return 0;
     }
@@ -35,7 +35,7 @@ int compare(unsigned char *a,unsigned char *b){
   return 1;
 }
 
-void breakSha(unsigned char *hash,unsigned char *hashString){
+void breakSha(unsigned char *hash,unsigned char *hashString,int nibbles){
   unsigned char *a = (unsigned char *) calloc((SHA_DIGEST_LENGTH * 2)+1,sizeof(unsigned char *));
   unsigned char *b = (unsigned char *) calloc((SHA_DIGEST_LENGTH * 2)+1,sizeof(unsigned char *));
   unsigned char *x0= (unsigned char *) calloc((SHA_DIGEST_LENGTH * 2)+1,sizeof(unsigned char *));
@@ -49,23 +49,23 @@ void breakSha(unsigned char *hash,unsigned char *hashString){
       printf("number of itration : %'d\r",i);
     }
     // a = H(a)
-    H(a,hash);
+    H(a,hash,nibbles);
     ShaToString(hash,hashString);
     copy(hashString,a);
     //=================================================
     // b = H(H(b))
-    H(b,hash);
+    H(b,hash,nibbles);
     ShaToString(hash,hashString);
-    H(hashString,hash);
+    H(hashString,hash,nibbles);
     ShaToString(hash,hashString);
     copy(hashString,b);
-      if(compare(a,b) == 1){
+    if(compare(a,b,nibbles) == 1){
 	printf("number of itration that we found the collision: %d\n",i);
 	break;
       }
     i++;
   }
-  if(compare(a,b) == 0){
+  if(compare(a,b,nibbles) == 0){
     printf("a and b are not equal\n");
     return;
   }
@@ -74,41 +74,62 @@ void breakSha(unsigned char *hash,unsigned char *hashString){
   unsigned char *ha = (unsigned char *) calloc((SHA_DIGEST_LENGTH * 2)+1,sizeof(unsigned char *));
   unsigned char *hb = (unsigned char *) calloc((SHA_DIGEST_LENGTH * 2)+1,sizeof(unsigned char *));
   while(1){
-    H(a,hash);
+    H(a,hash,nibbles);
     ShaToString(hash,hashString);
     copy(hashString,ha);
-    H(b,hash);
+    H(b,hash,nibbles);
     ShaToString(hash,hashString);
     copy(hashString,hb);
-    if(compare(ha,hb) ==1){
+    if(compare(ha,hb,nibbles) ==1){
       printf("find collision and strings are : \n");
-      print(a);
+      print(a,nibbles);
       printf("\n");
-      print(b);
+      print(b,nibbles);
       printf("\nand their hash is : ");
-      print(ha);
+      print(ha,nibbles);
       printf("\n");
       break;
     }else{
-      H(a,hash);
+      H(a,hash,nibbles);
       ShaToString(hash,hashString);
       copy(hashString,a);
-      H(b,hash);
+      H(b,hash,nibbles);
       ShaToString(hash,hashString);
       copy(hashString,b);
     }
   }
 }
 
-void main(){
-  unsigned char * hash = (unsigned char *) calloc(SHA_DIGEST_LENGTH,sizeof(unsigned char *));
-  //because we used %02x we need 2*Sha_size and because we close string with \0 i add +1 to the size 
+void main(int argc,char *argv[]){
+  //int nibbles;
+  if(argc == 1){
+    printf("program will run to find collision until 10 nibles\n");
+    int nibbles = 10;
+    unsigned char * hash = (unsigned char *) calloc(SHA_DIGEST_LENGTH,sizeof(unsigned char *));
+  //because we used %02x we need 2*Sha_size and because we close string with \0 i add +1 to the size                                                                                                                                                                               
   unsigned char * hashString = (unsigned char *) calloc((SHA_DIGEST_LENGTH * 2)+1,sizeof(unsigned char *));
-  //
-  H("3bab8ab772",hash);//initializing algorithm with mojtaba hash value
+  //you can init algorithm with any input                                                                                                                                                                                                                                          
+  //This program init with string that has length 10 if you want more you have to initialize it yourself                                                                                                                                                                           
+  H("3bab8ab772",hash,nibbles);//initializing algorithm with mojtaba hash value                                                                                                                                                                                                    
   ShaToString(hash,hashString);
   printf("program initialized with : ");
-  print(hashString);
+  print(hashString,nibbles);
   printf("\n");
-  breakSha(hash,hashString);
+  breakSha(hash,hashString,nibbles);
+  }
+  if(argc == 3){
+    int nibbles = atoi(argv[2]);
+    char *str = argv[1];
+    printf("you initilized algorithm with string:%s and number of nibbles you want is: %d\n",str,nibbles);
+    unsigned char * hash = (unsigned char *) calloc(SHA_DIGEST_LENGTH,sizeof(unsigned char *));
+  //because we used %02x we need 2*Sha_size and because we close string with \0 i add +1 to the size                                                                                                                                                                              
+  unsigned char * hashString = (unsigned char *) calloc((SHA_DIGEST_LENGTH * 2)+1,sizeof(unsigned char *));
+  //you can init algorithm with any input 
+  H(str,hash,nibbles);//initializing algorithm with mojtaba hash value                                                                                                                                                                                                   
+  ShaToString(hash,hashString);
+  printf("program initialized with : ");
+  print(hashString,nibbles);
+  printf("\n");
+  breakSha(hash,hashString,nibbles);
+  }
 }
